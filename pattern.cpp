7b6c1a8ad54fc51f1fde9b88d903b6caa74fcea8
Suman Moond <62123995+suman0771@users.noreply.github.com>
2022-04-20 22:58:47 +0530

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -111,6 +111,115 @@ cout<<"Pattern 5: \n";
     cout<<endl;
     i++;
   }
+  
+  
+  cout<<"Pattern 6: \n";
 
+    /*  n=4
+      16 15 14 13
+      12 11 10 9
+      8  7  6  5
+      4  3  2  1
+*/
+    i=1;
+    count=n*n;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout<<count<<" ";
+            count--;
+            j++;
+        }
+        i++;
+        cout<<endl;
+    }
+  
+   cout<<"Pattern 7: \n";
+
+    /*  n=4
+      *
+      * *
+      * * *
+      * * * *
+*/
+  i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            cout<<"* ";
+            j++;
+        }
+        i++;
+        cout<<endl;
+    }
+
+   cout<<"Pattern 8: \n";
+
+    /*  n=4
+      1
+      2 2
+      3 3 3
+      4 4 4 4
+*/
+
+  i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            cout<<i<<" ";
+            j++;
+        }
+        i++;
+        cout<<endl;
+    }
+  
+     cout<<"Pattern 9: \n";
+
+    /*  n=4
+      1
+      2 3
+      4 5 6
+      7 8 9 10
+*/
+    i=1;
+    count=1;
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            cout<<count<<" ";
+            count++;
+            j++;
+        }
+        i++;
+        cout<<endl;
+    }
+  
+   cout<<"Pattern 10: \n";
+
+    /*  n=4
+      1
+      2 3
+      3 4 5
+      4 5 6 7
+*/
+    i=1;
+    while(i<=n){
+        int j=1;
+        count=i;
+        while(j<=i){
+            cout<<count<<" ";
+            count++;
+            j++;
+        }
+        i++;
+        cout<<endl;
+    }
+  //or 
+
+  
+  
+  
+  
+  
 
 }
